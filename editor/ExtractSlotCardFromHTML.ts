e9fc521523eadd90cac698703d53bea91810dcaf
Minh Quy <onequy@gmail.com>
2023-09-04 12:47:49 +0700

    hehe

--- editor/ExtractSlotCardFromHTML.ts
+++ editor/ExtractSlotCardFromHTML.ts
@@ -1,12 +1,13 @@
 // const { parse } = require('node-html-parser');
 
 import parse from "node-html-parser";
+import { SlotName } from "../scr/Types";
 
 export function ExtractSlotCardFromHTML(htmlString: string) {
 
     const root = parse(htmlString);
 
-    // console.log(root.querySelectorAll('.d4t-list-affix.d4-color-gray')[1].text);
+     console.log(SlotName.Amulet, root.querySelectorAll('.d4t-list-affix.d4-color-gray')[1].text);
     
     return {
         slotName: '',
--- editor/js/editor/ExtractSlotCardFromHTML.js
+++ editor/js/editor/ExtractSlotCardFromHTML.js
@@ -3,9 +3,10 @@
 exports.__esModule = true;
 exports.ExtractSlotCardFromHTML = void 0;
 var node_html_parser_1 = require("node-html-parser");
+var Types_1 = require("../scr/Types");
 function ExtractSlotCardFromHTML(htmlString) {
     var root = (0, node_html_parser_1["default"])(htmlString);
-    // console.log(root.querySelectorAll('.d4t-list-affix.d4-color-gray')[1].text);
+    console.log(Types_1.SlotName.Amulet, root.querySelectorAll('.d4t-list-affix.d4-color-gray')[1].text);
     return {
         slotName: '',
         stats: []

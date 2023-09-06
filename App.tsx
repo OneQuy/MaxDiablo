0cdeb8e91c86c924071bab6a2e12ac748444c149
Minh Quy <onequy@gmail.com>
2023-09-06 08:42:05 +0700

    yahhhh

--- App.tsx
+++ App.tsx
@@ -21,7 +21,7 @@ import { FirebaseStorage_GetDownloadURLAsync, FirebaseStorage_UploadAsync } from
 import { FirebaseInit } from './scr/common/Firebase/Firebase';
 import { RequestCameraPermissionAsync, ToCanPrint } from './scr/common/UtilsTS';
 import { FontSize, Outline, windowSize } from './scr/AppConstant';
-import {CameraOptions, launchCamera} from 'react-native-image-picker';
+import { CameraOptions, launchCamera } from 'react-native-image-picker';
 import { ExtractSlotCard } from './scr/OCRUtils';
 
 function App(): JSX.Element {
@@ -37,66 +37,69 @@ function App(): JSX.Element {
     }
     else {
       const path = Platform.OS === 'android' ? 'file://' + response[0].realPath : response[0].path;
-      setUserImgUri(path)
-      
-      return
-      setStatus('Uploading...')
-
-      const tempFilePath = 'tmpfile-' + Date.now()
-      FirebaseInit()
-      const uplodaErr = await FirebaseStorage_UploadAsync(tempFilePath, path)
-
-      if (uplodaErr) {
-        console.error('upload file fail', uplodaErr);
-        setStatus('Upload failed: ' + ToCanPrint(uplodaErr))
-        return
-      }
-      else
-        console.log('success upload', tempFilePath);
-
-      const getURLRes = await FirebaseStorage_GetDownloadURLAsync(tempFilePath)
-
-      if (getURLRes.error) {
-        console.error('FirebaseStorage_GetDownloadURLAsync', getURLRes.error);
-        setStatus('GetURL Failed: ' + ToCanPrint(getURLRes.error))
-        return
-      }
-      else
-        console.log('get file url success', getURLRes.url);
-
-      await detectFromImgUrl(getURLRes.url)
+      onSelectedImg(path)
     }
   }, [])
 
   const onPressLogStatsFromTextOCRInClipboard = useCallback(async () => {
-    const res  = ExtractSlotCard(await Clipboard.getString())
+    const res = ExtractSlotCard(await Clipboard.getString())
 
     console.log(JSON.stringify(res, null, 1));
-    
+
   }, [])
 
   const onPressTakePhoto = useCallback(async () => {
     const camRequestRes = await RequestCameraPermissionAsync()
-    
+
     if (camRequestRes !== true) {
       Clipboard.setString('cam req ' + ToCanPrint(camRequestRes))
       return
     }
 
-    const result = await launchCamera({saveToPhotos: false} as CameraOptions)
+    const result = await launchCamera({ saveToPhotos: false } as CameraOptions)
 
     Clipboard.setString(ToCanPrint(result))
 
     if (!result || !result.assets)
       return
-    
+
     const path = result.assets[0].uri
 
     if (!path)
       return
 
-    setUserImgUri(path)
+    onSelectedImg(path)
   }, [])
+
+  const onSelectedImg = async (path: string) => {
+    setUserImgUri(path)
+
+    setStatus('Uploading...')
+
+    const tempFilePath = 'tmpfile-' + Date.now()
+    FirebaseInit()
+    const uplodaErr = await FirebaseStorage_UploadAsync(tempFilePath, path)
+
+    if (uplodaErr) {
+      console.error('upload file fail', uplodaErr);
+      setStatus('Upload failed: ' + ToCanPrint(uplodaErr))
+      return
+    }
+    else
+      console.log('success upload', tempFilePath);
+
+    const getURLRes = await FirebaseStorage_GetDownloadURLAsync(tempFilePath)
+
+    if (getURLRes.error) {
+      console.error('FirebaseStorage_GetDownloadURLAsync', getURLRes.error);
+      setStatus('GetURL Failed: ' + ToCanPrint(getURLRes.error))
+      return
+    }
+    else
+      console.log('get file url success', getURLRes.url);
+
+    await detectFromImgUrl(getURLRes.url)
+  }
 
   const detectFromImgUrl = async (imgUrl: string) => {
     const options = {
@@ -116,8 +119,17 @@ function App(): JSX.Element {
 
     try {
       const response = await axios.request(options);
-      console.log('-------------------');
-      console.log(response.data);
+
+      const result = response.data?.result
+
+      if (!result)
+        throw 'No have result'
+
+      // console.log(result);
+
+      const ocrText = ExtractSlotCard(result)
+      await Clipboard.setString(JSON.stringify(ocrText, null, 1))
+
       setStatus('SUCCESS')
     } catch (error) {
       console.error(error);
@@ -133,7 +145,7 @@ function App(): JSX.Element {
         <Text style={{ fontSize: 20, color: 'tomato', fontWeight: 'bold' }}>Diablo [IV] Senpai</Text>
         <Text style={{ fontSize: 15, color: 'white' }}>(version 0.0.1)</Text>
       </View>
-      <ScrollView style={{marginHorizontal: Outline.Margin}}>
+      <ScrollView style={{ marginHorizontal: Outline.Margin }}>
         {/* select photo btns */}
         <Text style={{ fontSize: 15, color: 'white', marginBottom: Outline.Margin }}>Chọn hình để rate:</Text>
         <View style={{ flexDirection: 'row', gap: Outline.Gap, justifyContent: 'center' }}>
@@ -150,7 +162,7 @@ function App(): JSX.Element {
           <View style={{ flex: 1 }}>
             {
               userImgUri === '' ? undefined :
-              <Image style={{ width: '100%', height: '100%' }} resizeMode='contain' source={{ uri: userImgUri }} />
+                <Image style={{ width: '100%', height: '100%' }} resizeMode='contain' source={{ uri: userImgUri }} />
             }
           </View>
           {/* info */}
@@ -158,6 +170,7 @@ function App(): JSX.Element {
 
           </View>
         </View>
+        <Text style={{ color: 'white' }}>{status}</Text>
         <Button title='Log stats from text OCR in Clipboard' onPress={onPressLogStatsFromTextOCRInClipboard} />
       </ScrollView>
     </SafeAreaView>

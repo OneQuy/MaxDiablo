d83456f94356ea2cea354692da44e108daa1b9bb
Minh Quy <onequy@gmail.com>
2023-09-24 11:23:33 +0700

    KnownWeirdStats

--- editor/js/GenerateAllStatName.js
+++ editor/js/GenerateAllStatName.js
@@ -2,34 +2,11 @@
 exports.__esModule = true;
 exports.GenerateAllStatName = void 0;
 var fs = require('fs');
-// export const GenerateAllStatName = () => {
-//     const text = fs.readFileSync('./assets/BuildsData.json');
-//     const buildsData: Tier[] = JSON.parse(text)
-//     const arr: string[] = []
-//     let loop = 0;
-//     for (let itier = 0; itier < buildsData.length; itier++) {
-//         const tier = buildsData[itier]
-//         for (let ibuild = 0; ibuild < tier.builds.length; ibuild++) {
-//             const build = tier.builds[ibuild]
-//             for (let islot = 0; islot < build.slots.length; islot++) {
-//                 const slot = build.slots[islot]
-//                 for (let istat = 0; istat < slot.stats.length; istat++) {
-//                     const stat = slot.stats[istat]
-//                     loop++
-//                     if (!arr.includes(stat.name)) {
-//                         arr.push(stat.name)
-//                     }
-//                 }
-//             }
-//         }
-//     }
-//     if (true)
-//         fs.writeFileSync('./assets/AllStats.json', JSON.stringify(arr, null, 1));
-//     else
-//         fs.writeFileSync('./assets/AllStats.json', JSON.stringify(arr));
-//     console.log('stat count', arr.length);
-//     console.log('loop', loop);
-// }
+var KnownWeirdStats = [
+    'Damage for X Seconds After Dodging an Attack',
+    'Max Evade Charges',
+    'Damage to Healthy Enemies',
+];
 var GenerateAllStatName = function () {
     var text = fs.readFileSync('./assets/ClassesData.json');
     var slots = JSON.parse(text);
@@ -48,10 +25,10 @@ var GenerateAllStatName = function () {
             }
         }
     }
-    // if (true)
+    // append KnownWeirdStats
+    arr = arr.concat(KnownWeirdStats);
+    // done
     fs.writeFileSync('./assets/AllStats.json', JSON.stringify(arr, null, 1));
-    // else
-    //     fs.writeFileSync('./assets/AllStats.json', JSON.stringify(arr));
     console.log('stat count', arr.length);
     console.log('loop', loop);
 };

d60917a087f98c2cae7c912ab16b54ee19abcfcc
Minh Quy <onequy@gmail.com>
2023-10-13 14:07:21 +0700

    log

--- scr/common/StorageLog.ts
+++ scr/common/StorageLog.ts
@@ -5,14 +5,14 @@ const Key = 'storage_log'
 var lines: string | undefined = undefined
 
 export const StorageLog_LogAsync = async (...args: any[]) => {
-    args.unshift(new Date())
+    // args.unshift(new Date())
     const s = args.join(' | ')
 
     await StorageLog_GetAsync() // load
 
     if (lines)
         lines += ('\n' + s)
-    else 
+    else
         lines = s
 
     await AsyncStorage.setItem(Key, lines)
@@ -25,7 +25,7 @@ export const StorageLog_GetAsync = async (): Promise<string> => {
     const res = await AsyncStorage.getItem(Key)
     lines = res === null ? '' : res
 
-    return lines
+    return (new Date() + '\n\n' + lines)
 }
 
 export const StorageLog_ClearAsync = async () => {

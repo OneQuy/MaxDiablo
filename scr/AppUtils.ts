fa67cd626f9538471eb6a74100d4a39d0d0b36d9
Minh Quy <onequy@gmail.com>
2023-10-29 12:26:53 +0700

    u unique

--- scr/AppUtils.ts
+++ scr/AppUtils.ts
@@ -17,6 +17,23 @@ export const GetSuitBuildsForUnique = (resultTxt: string, uniqueBuilds: UniqueBu
                 arr.push(build.name)
                 break
             }
+
+            // break in lines
+
+            const words = upperNameLower.split(' ')
+            let includeAll = true
+
+            for (let iword = 0; iword < words.length; iword++) {
+                if (!resultTxt.includes(words[iword])) {
+                    includeAll = false
+                    break
+                }
+            }
+
+            if (includeAll) {                
+                arr.push(build.name)
+                break
+            }
         }
     }
 

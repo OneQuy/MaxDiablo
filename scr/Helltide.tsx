03415e7946d21200b980147acf1607bbef00fb1d
Minh Quy <onequy@gmail.com>
2023-12-01 08:47:35 +0700

    mm

--- scr/Helltide.tsx
+++ scr/Helltide.tsx
@@ -5,30 +5,38 @@ import { windowSize } from './AppConstant'
 const AnimatedImageBackground = Animated.createAnimatedComponent(ImageBackground)
 
 const wholeMap = require('../assets/images/map/whole.jpg')
+const screenScale = Dimensions.get('screen').scale
 
 const Helltide = () => {
     const [mapRawSize, setMapRawSize] = useState<[number, number]>([10, 10])
     const [viewportSize, setViewportSize] = useState<[number, number]>([0, 0])
+    const mapPosition = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current
+    
+    // scale
+
     const mapScale = useRef(new Animated.Value(1)).current
+    const mapMinScale = useRef(0)
+
+    // functions
 
     const onLayoutViewport = (e: LayoutChangeEvent) => {
-        const scale = Dimensions.get('screen').scale
-
-        setViewportSize([e.nativeEvent.layout.width * scale, e.nativeEvent.layout.height * scale])
+        setViewportSize([e.nativeEvent.layout.width * screenScale, e.nativeEvent.layout.height * screenScale])
     }
 
     const onLoadedMap = (e: NativeSyntheticEvent<ImageLoadEventData>) => {
         Image.getSize(e.nativeEvent.source.uri, (w, h) => {
             if (w !== h)
                 console.warn('map w & h are diff. They should same!')
-                
+
             setMapRawSize([w, h])
 
             const viewportSizeMax = Math.max(viewportSize[0], viewportSize[1])
-
-            mapScale.setValue(viewportSizeMax / w)
-        })        
+            mapMinScale.current = viewportSizeMax / w
+            mapScale.setValue(mapMinScale.current)
+        })
     }
+
+    // render
 
     return (
         <SafeAreaView style={{ backgroundColor: 'gray', position: 'absolute', width: '100%', height: '100%', justifyContent: 'flex-start' }}>
@@ -41,6 +49,7 @@ const Helltide = () => {
                         source={wholeMap}
                         style={[
                             { width: mapRawSize[0], height: mapRawSize[1] },
+                            { ...mapPosition.getLayout() },
                             { transform: [{ scale: mapScale }] }]} />
                 </View>
             </View>
